063b67c87f1ae4b16faad501c1356d528b824257
massy <mashuriku@icloud.com>
2026-07-10 23:06:48 +0900

    Implement extension fee and late-night surcharge calculation

--- src/app/Services/CafeFeeCalculator.php
+++ src/app/Services/CafeFeeCalculator.php
@@ -14,12 +14,57 @@ class CafeFeeCalculator
     ): array {
         $basePrice = $courseType->price();
 
-        $tax = intval($basePrice * 0.1);
+        $totalinterval = $leaveAt->diff($enterAt);
 
-        $total = $basePrice + $tax;
+        $totalMinutes = ($totalinterval->days * 24 * 60)
+            + ($totalinterval->h * 60)
+            + $totalinterval->i;
+
+        $courseMinutes = $courseType->minutes();
+
+        $overTimeMinutes = max(0, $totalMinutes - $courseMinutes);
+
+        $extendStartTime = $enterAt->modify("+{$courseMinutes} minutes");
+
+        $nightTimeStart = $enterAt->setTime(22, 0);
+
+
+        if ($extendStartTime > $nightTimeStart) {
+            $nightExtensionStart = $extendStartTime;
+        } else {
+            $nightExtensionStart = $nightTimeStart;
+        }
+
+        if ($overTimeMinutes > 0 && $leaveAt > $nightTimeStart) {
+            $interval = $leaveAt->diff($nightExtensionStart);
+            $nightExtensionMinutes = ($interval->days * 24 * 60)
+                + ($interval->h * 60)
+                + $interval->i;
+        } else {
+            $nightExtensionMinutes = 0;
+        }
+
+        $numberOfNightExtensionCharges = (int) ceil($nightExtensionMinutes / 10);
+
+        $nightExtensionCharge = (int) ($numberOfNightExtensionCharges * 100 * 1.15);
+
+        $normalExtensionMinutes = $overTimeMinutes - $nightExtensionMinutes;
+
+        $numberOfNormalExtensionCharges = (int) ceil($normalExtensionMinutes / 10);
+
+        $extraCharge = (int) ($numberOfNormalExtensionCharges * 100);
+
+
+        $subtotal = $basePrice + $extraCharge + $nightExtensionCharge;
+
+        $tax = intval($subtotal * 0.1);
+
+        $total = $subtotal + $tax;
+
+
 
         return [
-            'subtotal' => $basePrice,
+            'subtotal' => $subtotal,
             'tax' => $tax,
             'total' => $total,
         ];
--- src/tests/Unit/CafeFeeCalculatorTest.php
+++ src/tests/Unit/CafeFeeCalculatorTest.php
@@ -36,8 +36,8 @@ class CafeFeeCalculatorTest extends TestCase
         // Arrange
         $calculator = new CafeFeeCalculator();
 
-        $enterAt = new DateTimeImmutable('2026-07-08 10:00:00');
-        $leaveAt = new DateTimeImmutable('2026-07-08 10:30:00');
+        $enterAt = new DateTimeImmutable('2026-07-08 18:00:00');
+        $leaveAt = new DateTimeImmutable('2026-07-08 21:00:00');
         $courseType = CourseType::THREE_HOURS;
 
         // Act
@@ -51,5 +51,71 @@ class CafeFeeCalculatorTest extends TestCase
         $this->assertSame(800, $result['subtotal']);
         $this->assertSame(80, $result['tax']);
         $this->assertSame(880, $result['total']);
+    }
+
+    public function test_calculate_three_hours_course_normal_extension_fee()
+    {
+        // Arrange
+        $calculator = new CafeFeeCalculator();
+
+        $enterAt = new DateTimeImmutable('2026-07-08 18:00:00');
+        $leaveAt = new DateTimeImmutable('2026-07-08 21:30:00');
+        $courseType = CourseType::THREE_HOURS;
+
+        // Act
+        $result = $calculator->calculate(
+            $enterAt,
+            $leaveAt,
+            $courseType
+        );
+
+        // Assert
+        $this->assertSame(1100, $result['subtotal']);
+        $this->assertSame(110, $result['tax']);
+        $this->assertSame(1210, $result['total']);
+    }
+
+    public function test_calculate_three_hours_course_not_extension_fee()
+    {
+        // Arrange
+        $calculator = new CafeFeeCalculator();
+
+        $enterAt = new DateTimeImmutable('2026-07-08 20:30:00');
+        $leaveAt = new DateTimeImmutable('2026-07-08 22:30:00');
+        $courseType = CourseType::THREE_HOURS;
+
+        // Act
+        $result = $calculator->calculate(
+            $enterAt,
+            $leaveAt,
+            $courseType
+        );
+
+        // Assert
+        $this->assertSame(800, $result['subtotal']);
+        $this->assertSame(80, $result['tax']);
+        $this->assertSame(880, $result['total']);
+    }
+
+    public function test_calculate_three_hours_course_night_extension_fee()
+    {
+        // Arrange
+        $calculator = new CafeFeeCalculator();
+
+        $enterAt = new DateTimeImmutable('2026-07-08 18:00:00');
+        $leaveAt = new DateTimeImmutable('2026-07-08 23:00:00');
+        $courseType = CourseType::THREE_HOURS;
+
+        // Act
+        $result = $calculator->calculate(
+            $enterAt,
+            $leaveAt,
+            $courseType
+        );
+
+        // Assert
+        $this->assertSame(2090, $result['subtotal']);
+        $this->assertSame(209, $result['tax']);
+        $this->assertSame(2299, $result['total']);
     }
 }
